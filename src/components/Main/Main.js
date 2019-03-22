1901da7f5140f050ccea956e6f6e232e628a814f
SanaChinaliyeva <sana.chinaliyeva@gmail.com>
2019-03-22 12:44:08 +0600

    Сделала компонент который рендерит нужную страницу

---
+++ src/components/Main/Main.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+import axios from '../../axios-pages';
+
+class Main extends Component{
+    state = {
+       currentPage: null
+    };
+    componentDidMount () {
+        this.getPageContent();
+    }
+    getPageContent = () => {
+       let page = this.props.page;
+       axios.get('pages.json').then(response => {
+           let pages = response.data;
+           let myPage = pages[page];
+           this.setState({currentPage: myPage});
+       });
+    };
+    render () {
+        return  this.state.currentPage ? (
+            <div className="Page">
+                <h1>{this.state.currentPage.title}</h1>
+                <p>{this.state.currentPage.content}</p>
+            </div>
+        ) : null
+    }
+}
+
+export default Main;
